307fbc71fd470974313f67c50fc8c41ce119b300
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2018-11-06 17:49:43 -0600

    uded somebody elses code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraOpMode.java
@@ -1,24 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.os.Bundle;
+import android.provider.MediaStore;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import static android.app.Activity.RESULT_OK;
+
 @Autonomous(name="cameraTest",group="Autonomous")
 public class CameraOpMode extends LinearOpMode {
     Camera cam = Camera.open(1);
-    public void initCamera() {
-        cam.setDisplayOrientation(0);
-        cam.startPreview();
-    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     public void takePicture() {
-        cam.takePicture(Camera.ShutterCallback,
-                null,
-                null,
-                Camera.PictureCallback);
-    }
-    public void runOpMode(){
 
+        private void dispatchTakePictureIntent(){
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                telemetry.addData(toString(bitmap.getPixel(50,50)));
+            }
+        }
+    }
+
+    public void runOpMode(){
+        takePicture();
     }
 }
