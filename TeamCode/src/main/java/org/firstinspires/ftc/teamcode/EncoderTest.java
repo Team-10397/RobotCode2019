f31c91d860faf2936c4fc895ca24991c1c3f1877
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2018-11-20 17:49:20 -0600

    changed to autonomusd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -1,31 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.io.InvalidClassException;
+
 @Autonomous(name = "Robot: Encoder test!", group = "TeleOp")
-public class EncoderTest extends OpMode {
+public class EncoderTest extends LinearOpMode {
     HardwareRobot iceRobot = new HardwareRobot();
 
-    public void init(){
+    public void runOpMode(){
         iceRobot.init(hardwareMap);
-    }
-    public void loop(){
-
         telemetry.addData("encoder left:",iceRobot.leftDrive.getCurrentPosition()/iceRobot.encoder_ticks_per_revolution);
         telemetry.addData("encoder right:",iceRobot.rightDrive.getCurrentPosition()/iceRobot.encoder_ticks_per_revolution);
-        if (gamepad1.dpad_up) {
-            iceRobot.encoderMove(12);
-        }
-        if (gamepad1.dpad_down) {
-            iceRobot.encoderMove(-12);
-        }
-        if (gamepad1.dpad_right) {
-            iceRobot.encoderTurn(90);
-        }
-        if (gamepad1.dpad_left) {
-            iceRobot.encoderTurn(-90);
+
+        int state = 0;
+        while (true) {
+            switch (state) {
+                case 0:
+                    iceRobot.moveTime(.5,0);
+                    state += 1;
+                    break;
+                case 1:
+                    if (iceRobot.leftDrive.getCurrentPosition() > 12*iceRobot.encoder_ticks_per_inch){
+                        state+=1;
+                    }
+                    break;
+                case 2:
+                    iceRobot.stop();
+                    state+=1;
+                    break;
+            }
         }
     }
 }
